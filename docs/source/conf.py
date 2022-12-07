b52cff58a420cb090a7e0635bf85450b1d1d96e1
mcc311 <daniel2000890311g@gmail.com>
2022-12-08 00:34:59 +0800

    finish p1, turn into v0.2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, mcc311'
 author = 'mcc311'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1'
+release = 'v0.2'
 
 # -- Const string settings ---------------------------------------------------
 
